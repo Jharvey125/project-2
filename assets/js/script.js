81d33b30c411631443f3eed83e21f616cf704250
Jharvey125 <jamieharvey125@outlook.com>
2022-01-08 13:29:10 +0000

    Add main javascript structure

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+const choices = ['rock','paper','scissors','lizard','spock'];
+
+function usersAnswer(event) {
+    let clickedButton = event.target;
+    let userChoice = clickedButton.className;
+    let computerChoice = computerAnswer();
+    renderChoices(userChoice, computerChoice);
+    let userWon = whoWon(userChoice, computerChoice);
+
+    renderResult(userWon);
+}
+
+function renderChoices(userChoice, computerChoice) {
+    document.querySelectorAll(".users-answer > .selection").forEach(selection => selection.style.display="none");
+    document.querySelector(`.users-answer > .${userChoice}`).style.display = "block";
+    document.querySelectorAll(".computers-answer > .selection").forEach(selection => selection.style.display="none");
+    document.querySelector(`.computers-answer > .${computerChoice}`).style.display = "block";
+}
+
+function computerAnswer() {
+    const choices = ['rock','paper','scissors','lizard','spock'];
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
+}
+
+function whoWon(userAnswer, computerAnswer) {
+    
+    if(computerAnswer === userAnswer) {
+        return;     
+    }else if(computerAnswer === 'scissors' && userAnswer === 'paper') {
+        return false;
+    }else if(computerAnswer === 'paper' && userAnswer === 'rock') {
+        return false
+    }else if(computerAnswer === 'rock' && userAnswer === 'lizard') {
+        return false
+    }else if(computerAnswer === 'lizard' && userAnswer === 'spock') {
+        return false
+    }else if(computerAnswer === 'spock' && userAnswer === 'scissors') {
+        return false
+    }else if(computerAnswer === 'scissors' && userAnswer === 'lizard') {
+        return false
+    }else if(computerAnswer === 'lizard' && userAnswer === 'paper') {
+        return false
+    }else if(computerAnswer === 'paper' && userAnswer === 'spock') {
+        return false
+    }else if(computerAnswer === 'spock' && userAnswer === 'rock') {
+        return false
+    }else if(computerAnswer === 'rock' && userAnswer === 'scissors') {
+        return false
+    }else (computerAnswer < userAnswer)
+        return true;
+}
+
+function incrementScore() {
+    let oldscore = parseInt(document.getElementById('user-won').innerText);
+    document.getElementById("user-won").innerText = ++oldscore;
+}
+   
+function incrementWrongScore() {
+    let oldscore = parseInt(document.getElementById("user-lost").innerText);
+    document.getElementById("user-lost").innerText = ++oldscore;
+}
+
+function renderResult(userWon) {
+    if (userWon == undefined) {
+        alert('draw, no points');
+    } else if (userWon) {
+        alert('you won')
+        incrementScore();
+    } else {
+        alert('you lose')
+        incrementWrongScore();
+    }
+}
+
+document.querySelector(".controls-area").addEventListener("click", usersAnswer);
